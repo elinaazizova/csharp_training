75c12896763a620f6e85f048936c7a25d5e7db7c
Azizova, Elina <lina-azizova@yandex.ru>
2018-04-04 12:36:58 +0300

    change name

--- addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
@@ -7,7 +7,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace WebAddressbookTests
+namespace WebAddressbookTesting
 {
     [TestFixture]
     public class GroupCreationTests
